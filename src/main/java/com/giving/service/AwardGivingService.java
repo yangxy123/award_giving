6557c36448e9e81274f044ffdf35232126d5cf42
yangxy <276695072@qq.com>
2026-01-13 16:50:55 +0800

    增加越南彩28组和泰国彩验奖逻辑

--- src/main/java/com/giving/service/AwardGivingService.java
+++ src/main/java/com/giving/service/AwardGivingService.java
@@ -8,10 +8,26 @@ import com.giving.req.NoticeReq;
 */
 public interface AwardGivingService {
 	/**
-	 * 通知派奖
+	 * 通知派奖（越南彩18组）
 	* @author yangxy
 	* @version 创建时间：2025年12月30日 下午5:09:24 
 	* @param noticeReq
 	 */
 	public void notice(NoticeReq noticeReq);
+	
+	/**
+	 * 通知派奖（越南彩28组）
+	* @author yangxy
+	* @version 创建时间：2025年12月30日 下午5:09:24 
+	* @param noticeReq
+	 */
+	public void noticeNorth(NoticeReq noticeReq);
+	
+	/**
+	 * 通知派奖（泰国彩）
+	* @author yangxy
+	* @version 创建时间：2025年12月30日 下午5:09:24 
+	* @param noticeReq
+	 */
+	public void noticeTh(NoticeReq noticeReq);
 }
--- src/main/java/com/giving/service/impl/AwardGivingServiceImpl.java
+++ src/main/java/com/giving/service/impl/AwardGivingServiceImpl.java
@@ -37,6 +37,8 @@ public class AwardGivingServiceImpl implements AwardGivingService {
 	public void notice(NoticeReq noticeReq) {
 		int pageSize = 3000;
 		int pageNo = 0;
+		List<String> codeList = Lists.newArrayList(noticeReq.getCode().split(","));
+		int maxSize = codeList.size() - 1;
 		while (true) {
 			PageHelper.startPage(pageNo, pageSize);
 			// TODO Auto-generated method stub
@@ -51,8 +53,6 @@ public class AwardGivingServiceImpl implements AwardGivingService {
 			}
 			pageNo +=1;
 			// 将开奖号码转换为list
-			List<String> codeList = Lists.newArrayList(noticeReq.getCode().split(","));
-			int maxSize = codeList.size() - 1;
 			List<BetInfoEntity> allWinList = Lists.newArrayList();
 			List<Integer> endList = Lists.newArrayList();
 			
@@ -352,5 +352,278 @@ public class AwardGivingServiceImpl implements AwardGivingService {
 		             ),
 		             map -> new ArrayList<>(map.values())
 		         ));
+	}
+
+	@Override
+	public void noticeNorth(NoticeReq noticeReq) {
+		// TODO Auto-generated method stub
+		int pageSize = 3000;
+		int pageNo = 0;
+		// 将开奖号码转换为list
+		List<String> codeList = Lists.newArrayList(noticeReq.getCode().split(","));
+		int maxSize = codeList.size() - 1;
+		while (true) {
+			PageHelper.startPage(pageNo, pageSize);
+			// TODO Auto-generated method stub
+			// 获取对应奖期对应彩种未撤单且未派奖的所有订单
+			Page<BetInfoEntity> page = (Page<BetInfoEntity>)betInfoMapper.selectList(
+					new QueryWrapper<BetInfoEntity>().lambda().eq(BetInfoEntity::getIssue, noticeReq.getIssue())
+							.eq(BetInfoEntity::getLotteryId, noticeReq.getLotteryId()).eq(BetInfoEntity::getIsCancel, 0)
+							.eq(BetInfoEntity::getPrizeStatus, 0));
+			List<BetInfoEntity> list = page.getResult();
+			if(list.isEmpty()) {
+				break;
+			}
+			pageNo +=1;
+			List<BetInfoEntity> allWinList = Lists.newArrayList();
+			List<Integer> endList = Lists.newArrayList();
+			
+			new Thread(()->{//2D头,尾，头尾玩法
+				try {
+					List<BetInfoEntity> betList = list.stream().filter(vo -> vo.getMethodId() == "2D头" 
+							|| vo.getMethodId() == "2D尾" 
+							|| vo.getMethodId() == "2D头尾")
+							.collect(Collectors.toList());
+					List<String> headCodeList = codeList.stream().limit(4).collect(Collectors.toList());
+					String endCode = codeList.get(maxSize).substring(3,5);
+					for(String code:headCodeList) {
+						List<BetInfoEntity> collect = betList.stream().filter(vo->vo.getCode().indexOf(code)>=0 && (vo.getMethodId() == "2D头尾" || vo.getMethodId() == "2D头")).collect(Collectors.toList());
+						allWinList.addAll(collect);
+					}
+					List<BetInfoEntity> collect = betList.stream().filter(vo->vo.getCode().indexOf(endCode)>=0 && (vo.getMethodId() == "2D头尾" || vo.getMethodId() == "2D尾")).collect(Collectors.toList());
+					allWinList.addAll(collect);
+				}catch (Exception e) {
+					// TODO: handle exception
+					e.printStackTrace();
+				}
+				endList.add(1);
+			}).start();
+			
+			new Thread(()->{//3D头,尾，头尾玩法
+				try {
+					List<BetInfoEntity> betList = list.stream().filter(vo -> vo.getMethodId() == "3D头" 
+							|| vo.getMethodId() == "3D尾" 
+							|| vo.getMethodId() == "3D头尾")
+							.collect(Collectors.toList());
+					List<String> headCodeList = codeList.stream().skip(4).limit(3).collect(Collectors.toList());
+					String endCode = codeList.get(maxSize).substring(2,5);
+					for(String code:headCodeList) {
+						List<BetInfoEntity> collect = betList.stream().filter(vo->vo.getCode().indexOf(code)>=0 && (vo.getMethodId() == "3D头尾" || vo.getMethodId() == "3D头")).collect(Collectors.toList());
+						allWinList.addAll(collect);
+					}
+					List<BetInfoEntity> collect = betList.stream().filter(vo->vo.getCode().indexOf(endCode)>=0 && (vo.getMethodId() == "3D头尾" || vo.getMethodId() == "3D尾")).collect(Collectors.toList());
+					allWinList.addAll(collect);
+				}catch (Exception e) {
+					// TODO: handle exception
+					e.printStackTrace();
+				}
+				endList.add(2);
+			}).start();
+			
+			new Thread(()->{//4D尾玩法
+				try {
+					List<BetInfoEntity> betList = list.stream().filter(vo -> vo.getMethodId() == "4D尾" )
+							.collect(Collectors.toList());
+					String endCode = codeList.get(maxSize).substring(1,5);
+					List<BetInfoEntity> collect = betList.stream().filter(vo->vo.getCode().indexOf(endCode)>=0).collect(Collectors.toList());
+					allWinList.addAll(collect);
+				}catch (Exception e) {
+					// TODO: handle exception
+					e.printStackTrace();
+				}
+				endList.add(3);
+			}).start();
+			
+			new Thread(()->{//包组玩法
+				try {
+					Map<String, Long> countMap = codeList.stream()
+							.collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+					List<BetInfoEntity> betList = list.stream().filter(vo -> vo.getMethodId() == "4D包组" 
+							||vo.getMethodId() == "3D包组" 
+							||vo.getMethodId() == "2D包组" )
+							.collect(Collectors.toList());
+					for(String key:countMap.keySet()) {
+						Long multiple = countMap.get(key);
+
+						if (key.length() == 2) {
+							List<BetInfoEntity> winList = betList.stream()
+									.filter(vo -> vo.getCode().indexOf(key + ",") >= 0
+											|| vo.getCode().endsWith(key) && vo.getMethodId() == "2d包组玩法")
+									.collect(Collectors.toList());
+							winList.forEach(vo -> {
+								vo.setBonus(vo.getBonus() * multiple);
+							});
+							allWinList.addAll(winList);
+						} else if (key.length() == 3) {
+							List<BetInfoEntity> winList = betList.stream()
+									.filter(vo -> vo.getCode().indexOf(key + ",") >= 0 || vo.getCode().endsWith(key)
+											|| (vo.getCode()
+													.indexOf(key.substring(key.length() - 2, key.length()) + ",") >= 0
+													&& vo.getMethodId() == "2d包组玩法")
+											|| (vo.getCode().endsWith(key.substring(key.length() - 2, key.length()))
+													&& vo.getMethodId() == "3d包组玩法"))
+									.collect(Collectors.toList());
+							winList.forEach(vo -> {
+								vo.setBonus(vo.getBonus() * multiple);
+							});
+							allWinList.addAll(winList);
+						} else {
+							List<BetInfoEntity> winList = betList.stream()
+									.filter(vo -> (vo.getCode()
+											.indexOf(key.substring(key.length() - 2, key.length()) + ",") >= 0
+											&& vo.getMethodId() == "2d包组玩法")
+											|| (vo.getCode()
+													.endsWith(key.substring(key.length() - 2, key.length()))
+													&& vo.getMethodId() == "2d包组玩法")
+											|| (vo.getCode()
+													.indexOf(key.substring(key.length() - 3, key.length()) + ",") >= 0
+													&& vo.getMethodId() == "3d包组玩法")
+											|| (vo.getCode().endsWith(key.substring(key.length() - 3, key.length()))
+													&& vo.getMethodId() == "3d包组玩法")
+											|| vo.getCode()
+													.indexOf(key.substring(key.length() - 4, key.length()) + ",") >= 0
+											|| vo.getCode().endsWith(key.substring(key.length() - 4, key.length())))
+									.collect(Collectors.toList());
+							winList.forEach(vo -> {
+								vo.setBonus(vo.getBonus() * multiple);
+							});
+							allWinList.addAll(winList);
+						}
+					}
+				}catch (Exception e) {
+					// TODO: handle exception
+					e.printStackTrace();
+				}
+				endList.add(4);
+			}).start();
+			
+			new Thread(()->{//pl2玩法
+				try {
+					// 筛选出pl2玩法的订单
+					List<BetInfoEntity> betList = list.stream().filter(vo -> vo.getMethodId() == 7)
+							.collect(Collectors.toList());
+					List<BetInfoEntity> winList = betList.stream()
+							.filter(vo -> noticeReq.getCode().indexOf(vo.getCode().split(",")[0] + ",") >= 0
+									&& (noticeReq.getCode().indexOf(vo.getCode().split(",")[0] + ",") < noticeReq
+											.getCode().indexOf(vo.getCode().split(",")[1] + ",")
+											|| noticeReq.getCode().endsWith(vo.getCode().split(",")[1])))
+							.collect(Collectors.toList());
+					allWinList.addAll(winList);
+				}catch (Exception e) {
+					// TODO: handle exception
+					e.printStackTrace();
+				}
+				endList.add(5);
+			}).start();
+			
+			new Thread(()->{//pl3玩法
+				try {
+					// 筛选出pl2玩法的订单
+					List<BetInfoEntity> betList = list.stream().filter(vo -> vo.getMethodId() == 7)
+							.collect(Collectors.toList());
+					List<BetInfoEntity> winList = betList.stream().filter(vo -> noticeReq.getCode()
+							.indexOf(vo.getCode().split(",")[0] + ",") >= 0
+							&& noticeReq.getCode().indexOf(vo.getCode().split(",")[1] + ",") >= 0
+							&& (noticeReq.getCode().indexOf(vo.getCode().split(",")[0] + ",") < noticeReq.getCode()
+									.indexOf(vo.getCode().split(",")[1] + ",")
+									&& (noticeReq.getCode().indexOf(vo.getCode().split(",")[1] + ",") < noticeReq
+											.getCode().indexOf(vo.getCode().split(",")[2] + ",")
+											|| noticeReq.getCode().endsWith(vo.getCode().split(",")[2]))))
+							.collect(Collectors.toList());
+					allWinList.addAll(winList);
+				}catch (Exception e) {
+					// TODO: handle exception
+					e.printStackTrace();
+				}
+				endList.add(5);
+			}).start();
+
+			//判断所有子线程是否执行完成
+			while (true) {
+				if (endList.size() == 5) {
+					break;
+				}
+				Thread.sleep(10);
+			}
+			//中奖订单
+			List<BetInfoEntity> sumList = getSumList(allWinList);
+			//中奖订单ID列表
+			List<String> winIdList = sumList.stream().map(BetInfoEntity::getProjectId).collect(Collectors.toList());
+			//未中奖订单ID
+			List<BetInfoEntity> notWinList = list.stream().filter(vo -> !winIdList.contains(vo.getProjectId()))
+					.collect(Collectors.toList());
+		}
+	}
+
+	@Override
+	public void noticeTh(NoticeReq noticeReq) {
+		// TODO Auto-generated method stub
+		int pageSize = 3000;
+		int pageNo = 0;
+		// 将开奖号码转换为list
+		List<String> codeList = Lists.newArrayList(noticeReq.getCode().split(","));
+		int maxSize = codeList.size() - 1;
+		while (true) {
+			PageHelper.startPage(pageNo, pageSize);
+			// TODO Auto-generated method stub
+			// 获取对应奖期对应彩种未撤单且未派奖的所有订单
+			Page<BetInfoEntity> page = (Page<BetInfoEntity>)betInfoMapper.selectList(
+					new QueryWrapper<BetInfoEntity>().lambda().eq(BetInfoEntity::getIssue, noticeReq.getIssue())
+							.eq(BetInfoEntity::getLotteryId, noticeReq.getLotteryId()).eq(BetInfoEntity::getIsCancel, 0)
+							.eq(BetInfoEntity::getPrizeStatus, 0));
+			List<BetInfoEntity> list = page.getResult();
+			if(list.isEmpty()) {
+				break;
+			}
+			pageNo +=1;
+			List<BetInfoEntity> allWinList = Lists.newArrayList();
+			List<Integer> endList = Lists.newArrayList();
+			
+			new Thread(()->{//1D头、2D头、3D头、1D尾，2D尾
+				try {
+					List<BetInfoEntity> betList = list.stream().filter(vo->vo.getMethodId()==7).collect(Collectors.toList());
+					String headCode = codeList.get(0).substring(3,6);
+					String headCode1 = codeList.get(0).substring(4,6);
+					String endCode = codeList.get(maxSize);
+					
+					List<BetInfoEntity> collect = betList.stream().filter(vo-> (headCode.indexOf(vo.getCode()) >= 0 && (vo.getMethodId() == "3D头" || vo.getMethodId()=="1D头"))
+							|| (headCode1.indexOf(vo.getCode()) >= 0 && vo.getMethodId() == "2D头")
+							|| (endCode.indexOf(vo.getCode()) && (vo.getMethodId() == "2D尾" || vo.getMethodId()=="1D尾"))).collect(Collectors.toList());
+					allWinList.addAll(collect);
+				}catch (Exception e) {
+					// TODO: handle exception
+				}
+				endList.add(1);
+			}).start();
+			
+			new Thread(()->{//3D前三、3D后三
+				try {
+					List<BetInfoEntity> betList = list.stream().filter(vo->vo.getMethodId()==7).collect(Collectors.toList());
+					String frontThreeCode = codeList.get(1)+codeList.get(2);
+					String afterThreeCode = codeList.get(3)+codeList.get(4);
+					
+					List<BetInfoEntity> collect = betList.stream().filter(vo-> ((frontThreeCode.startsWith(vo.getCode()) || frontThreeCode.endsWith(vo.getCode())) && vo.getMethodId() == "3D前三")
+							|| ((afterThreeCode.startsWith(vo.getCode()) || afterThreeCode.endsWith(vo.getCode())) && vo.getMethodId() == "3D后三")).collect(Collectors.toList());
+					allWinList.addAll(collect);
+				}catch (Exception e) {
+					// TODO: handle exception
+				}
+				endList.add(2);
+			}).start();
+			
+			//判断所有子线程是否执行完成
+			while (true) {
+				if (endList.size() == 2) {
+					break;
+				}
+				Thread.sleep(10);
+			}
+			//中奖订单 allWinList
+			//中奖订单ID列表
+			List<String> winIdList = allWinList.stream().map(BetInfoEntity::getProjectId).collect(Collectors.toList());
+			//未中奖订单ID
+			List<BetInfoEntity> notWinList = list.stream().filter(vo -> !winIdList.contains(vo.getProjectId()))
+					.collect(Collectors.toList());
+		}
 	}
 }
