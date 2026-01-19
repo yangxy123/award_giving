257e9ae012cf81ad18a38eb52412f72542df41bd
Burunet <2030676100@qq.com>
2026-01-19 15:56:06 +0800

    新增全 录号

--- src/main/java/com/giving/service/impl/AwardingProcessServiceImpl.java
+++ src/main/java/com/giving/service/impl/AwardingProcessServiceImpl.java
@@ -110,6 +110,13 @@ public class AwardingProcessServiceImpl implements AwardingProcessService {
         //  5.验派后出现的金额变化写入账变表(cn007_orders)，并修改用户余额(cn007_user_fund)
     }
 
+    //根据采种方法执行对应的验证流程
+
+    /**
+     *
+     * @param roomMaster
+     * @param issueInfo
+     */
     public void lotteryDraw(RoomMasterEntity roomMaster,IssueInfoEntity issueInfo){
         LotteryEntity lottery = lotteryMapper.selectById(issueInfo.getLotteryId());
         new Thread(() -> {
@@ -120,17 +127,25 @@ public class AwardingProcessServiceImpl implements AwardingProcessService {
             n.setIssue(issueInfo.getIssue());
             n.setCode(issueInfo.getCode());
             n.setLotteryId(issueInfo.getLotteryId());
-            //18--越南自开
-            //泰国
             switch (lottery.getFunctionType()){
                 case "VN_S":
-                case "VN_C":
-                    //18--越南自开
+                case "VN_C": //18--越南自开
                     awardGivingService.notice(n);
                     break;
-                case "VN_N":
-//                        28-组
+                case "VN_N"://28-组 越南北部
                     awardGivingService.noticeNorth(n);
+                    break;
+                case "TH":      //泰国彩
+                case "TH_30S":  //泰国分分彩
+                    awardGivingService.noticeTh(n);
+                    break;
+                case "LA":  //老挝彩
+                case "MY":  //马来西亚彩
+                    awardGivingService.noticeLw(n);
+                    break;
+//                case "FC3D":  //加拿大1分3D  亚洲30秒3D
+                case "K3":  //亚洲30秒快3 亚洲1分快3  澳洲5分快3
+                    awardGivingService.noticeKs(n);
                     break;
             }
         }).start();
